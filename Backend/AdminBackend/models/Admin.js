6006d1a5e5881e9d81c73f301f74b8e6825603b6
saymi313 <saymi.313@gmail.com>
2024-12-07 22:05:12 +0500

    Feedback added

--- Backend/AdminBackend/models/Admin.js
+++ Backend/AdminBackend/models/Admin.js
@@ -1,8 +1,8 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const adminSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-// });
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-// module.exports = mongoose.model("Admin", adminSchema);
+module.exports = mongoose.model("Admin", adminSchema);
